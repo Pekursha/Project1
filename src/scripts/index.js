1e64f5070193780ea9ef9f455df89bd5a15bf297
Maria Ilina <pekurma@gmail.com>
2024-04-21 12:59:40 +0700

    Added my Sprint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,28 @@
+let ageTitle=document.getElementById("age");console.log("Hello world");
 
-console.log("Hello world");
+  window.onload = () => { 
+    const header = document.querySelector('header'); 
+     
+    document.addEventListener('scroll', event => { 
+        console.log(event.target.scrollTop) 
+    }); 
+     
+    const smoothScrollLinks = document.querySelectorAll('.smooth-scroll'); 
+     
+    for (let link of smoothScrollLinks) { 
+      link.addEventListener('click', event => { 
+         event.preventDefault(); 
+         
+        const target = event.target; 
+        const elementToScroll = document.querySelector(target.getAttribute('href')); 
+        elementToScroll.scrollIntoView({ behavior: 'smooth', block: 'center'}); 
+      }); 
+    } 
+  }; // Работает!!! 
+
+  // Для кнопки
+  const button = document.querySelector('.click-btn');
+
+  button.addEventListener("click", (event) => {
+    button.textContent = "отправлено";
+  });
